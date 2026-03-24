31e3f33e14d6f39ce18ef82dda39ad2f73c51308
Baatar-line <sbatmagnai25@gmail.com>
2026-03-24 17:44:36 +0800

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,36 @@
+import { Input, Tod } from "@/components/Todo";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <div
+        className=" flex flex-col min-w-[377px] min-h-[290px] bg-white
+     text-black rounded-2xl justify-center items-center text-xs gap-5"
+      >
+        {" "}
+        <h1 className="text-cemibold text-xl">To-Do list</h1>
+        <div className="flex w-[345px] min-h-[38px] justify-between gap-1">
+          <Input />
+          <div className="border-white bg-blue-600 border rounded-xl p-1 px-4">
+            add
+          </div>
+        </div>
+        <div className="flex gap-3 border-black ">
+          {" "}
+          <p className=" border rounded-xl py-2 px-4 bg-blue-600">All</p>
+          <p className=" border rounded-xl py-2 px-4">Active</p>
+          <p className="border rounded-xl py-2 px-4">Completed</p>{" "}
+        </div>
+        <p>No tasks yet. Add one above!</p>
+        <div className="flex flex-col justify-center items-center">
+        {" "}
+        <p>Powered by</p>
+        <p>Pinecone academy</p>
+      </div>
+      </div>
+
+      
+    </div>
+  );
+}
---
+++ src/components/Todo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Input = () => {
+  return (
+    <input className='border
+    rounded-xl w-4xl
+    ' placeholder='type here!'/>
+  )
+}
